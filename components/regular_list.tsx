b9b6457a6ec268c57c550141960e292279e92bed
Nymosdt <dc377303@gmail.com>
2024-08-19 15:42:23 +0000

    build

--- components/regular_list.tsx
+++ components/regular_list.tsx
@@ -1,42 +1,58 @@
-import Link from "next/link"
+import React from "react";
+import Link from "next/link";
 
-export const ListItem = ({items , resourcename , component : Component , className , onClick , withPopup = false , href , id} : {
-    items : any[] , 
-    resourcename : string , 
-    component : React.ElementType , 
-    className : string ,
-    onClick ?: () => void,
-    withPopup ?: boolean,
-    href ?: string,
-    id ?:  string,
+export const ListItem = ({
+    items = [], // Assurez-vous que 'items' est un tableau par défaut
+    resourcename,
+    component: Component,
+    className,
+    onClick,
+    withPopup = false,
+    href,
+    id,
+}: {
+    items?: any[], // Type optionnel pour 'items'
+    resourcename: string,
+    component: React.ElementType,
+    className: string,
+    onClick?: () => void,
+    withPopup?: boolean,
+    href?: string,
+    id?: string,
 }) => {
-    
+    // Assurez-vous que 'items' est un tableau avant d'utiliser '.map()'
+    if (!Array.isArray(items)) {
+        console.error("Expected 'items' to be an array, but received:", items);
+        return null;
+    }
+
     return (
         <div className={className}>
-            {items.map((item , i ) => (
-                <>
+            {items.map((item, i) => (
+                <React.Fragment key={i}>
                     {
-                        !withPopup ? <Component 
-                        key={i} 
-                        {...{[resourcename]: item}} 
-                        onClick={onClick}
-                />  : 
-                    <Link href={{
-                        pathname : `${href}`,
-                        query: {
-                            id : id
-                        },
-                    }}>
-                        <Component 
-                        key={i} 
-                        {...{[resourcename]: item}} 
-                        onClick={onClick}
-                        /> 
-                    </Link>
+                        !withPopup ? 
+                            <Component 
+                                {...{ [resourcename]: item }} 
+                                onClick={onClick}
+                            /> 
+                            : 
+                            <Link 
+                                href={{
+                                    pathname: `${href}`,
+                                    query: {
+                                        id: id
+                                    },
+                                }}
+                            >
+                                <Component 
+                                    {...{ [resourcename]: item }} 
+                                    onClick={onClick}
+                                /> 
+                            </Link>
                     }
-                </>
+                </React.Fragment>
             ))}
         </div>
-    )
-}
-
+    );
+};
